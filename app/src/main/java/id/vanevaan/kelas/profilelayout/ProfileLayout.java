e1736cc059f5fdbcfe59871bfa1c72c07d9828be
vanevaan <rossievan46@gmail.com>
2020-10-14 01:26:59 +0700

    First Commit

---
+++ app/src/main/java/id/vanevaan/kelas/profilelayout/ProfileLayout.java
@@ -0,0 +1,14 @@
+package id.vanevaan.kelas.profilelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_layout);
+    }
+}
